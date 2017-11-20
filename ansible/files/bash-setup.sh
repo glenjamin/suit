36959ddc717ad58b5132cdea79b868ca56d6f7bb
Glen Mailer <glen@stainlessed.co.uk>
2017-11-20 23:07:11 +0000

    More bash tweaks

--- ansible/files/bash-setup.sh
+++ ansible/files/bash-setup.sh
@@ -102,7 +102,6 @@ xterm*|rxvt*)
 *)
     ;;
 esac
-unset prompt_host
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
@@ -131,6 +130,9 @@ alias l='ls -CF'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Muscle memory is a terrible thing
+alias sub=atom
 
 # List processes via pgrep, then prompt to pkill
 pgk() {
