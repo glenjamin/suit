636756b77ae6b88549d5f556ea77fad9f9b0d693
Glen Mailer <glen@circleci.com>
2019-08-29 17:51:52 +0100

    More stuff

--- all-ansible/files/bash-setup.sh
+++ all-ansible/files/bash-setup.sh
@@ -136,7 +136,7 @@ alias l='ls -CF'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # Muscle memory is a terrible thing
-alias sub=code
+alias sub='code -g'
 
 # List processes via pgrep, then prompt to pkill
 pgk() {
