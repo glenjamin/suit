fe53c4c60fc35ff0943490cb46a340edab2e736d
Glen Mailer <glen@geckoboard.com>
2025-06-04 22:41:59 +0100

    more fish stuff

--- all-ansible/files/fish/config.fish
+++ all-ansible/files/fish/config.fish
@@ -1,5 +1,15 @@
 alias sub='code -g'
 
+# Attempt to import any env vars exported from profile.d
+for file in /etc/profile.d/*.sh
+  grep 'export' $file | sed -e 's/export/set -x/' -e 's/=/ /' | source
+end
+
+# Like conf.d, but these are in git
+for file in ~/.config/fish/shared.conf.d/*.fish
+  source $file
+end
+
 if status is-interactive
 
 end
--- all-ansible/files/fish/shared.conf.d/atuin.fish
+++ all-ansible/files/fish/shared.conf.d/atuin.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    if which atuin
+    if type -q atuin
         atuin init fish --disable-up-arrow | source
     end
 end
