09490cbbfb31729bc8a49f014f3c4255d9748f67
Glen Mailer <glen@geckoboard.com>
2025-06-05 08:29:31 +0000

    that fix profile.d thing didn't work well

--- all-ansible/files/fish/config.fish
+++ all-ansible/files/fish/config.fish
@@ -1,10 +1,5 @@
 alias sub='code -g'
 
-# Attempt to import any env vars exported from profile.d
-for file in /etc/profile.d/*.sh
-  grep 'export' $file | sed -e 's/export/set -x/' -e 's/=/ /' | source
-end
-
 # Like conf.d, but these are in git
 for file in ~/.config/fish/shared.conf.d/*.fish
   source $file
